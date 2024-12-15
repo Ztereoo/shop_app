abf18507b2cee54fb9572588b2f9256ea09fd011
stereo <airstarview@gmail.com>
2024-12-15 19:15:28 +0300

    users table added to DB

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -3,7 +3,9 @@ __all__ =(
     "DatabaseHelper",
     "db_helper",
     "Product",
+    "User",
 )
 from .base import Base
 from .product import Product
+from .user import User
 from .db_helper import DatabaseHelper, db_helper
