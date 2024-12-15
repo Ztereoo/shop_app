d3b0c8bd5eee26ea743275cd30ff7d18243fa13f
stereo <airstarview@gmail.com>
2024-12-15 19:01:15 +0300

    no need create all on app start

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 from contextlib import asynccontextmanager
-from typing import Annotated
+
 import uvicorn
 
 from core.config import settings
-from core.models import Base,db_helper
 from fastapi import FastAPI,Path
-from pydantic import EmailStr, BaseModel
 from api_v1 import router as router_v1
 from items_views import router as items_router
 from users.views import router as users_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    async with db_helper.engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+    # async with db_helper.engine.begin() as conn:
+    #     await conn.run_sync(Base.metadata.create_all)
     yield
 
 
